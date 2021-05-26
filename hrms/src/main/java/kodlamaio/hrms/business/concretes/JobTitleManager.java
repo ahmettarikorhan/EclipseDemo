405de6151074d6f8510730ceb9581dc09ef18eaf
Ahmet Tarık <79899050+mythredd@users.noreply.github.com>
2021-05-26 17:42:53 +0300

    Add files via upload

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobTitleManager.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.dataAccess.abstracts.JobTitleDao;
+import kodlamaio.hrms.entities.concretes.JobTitle;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+
+@Service
+public class JobTitleManager implements JobTitleService{
+	
+	private JobTitleDao jobTitleDao;
+	
+	@Autowired
+	public JobTitleManager(JobTitleDao jobTitleDao) {
+		super();
+		this.jobTitleDao = jobTitleDao;
+	}
+
+	@Override
+	public DataResult<List<JobTitle>> getAll() {
+		return new SuccessDataResult<List<JobTitle>>(this.jobTitleDao.findAll(),"Data Listelendi");
+	}
+	
+
+	@Override
+	public Result add(JobTitle jobTitle) {
+		this.jobTitleDao.save(jobTitle);
+		return new SuccessResult("İş eklendi");
+	}
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employee.java
@@ -0,0 +1,113 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="birth_date")
+	private String birthDate;
+	
+	@Column(name="e_mail")
+	private String eMail;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="password_again")
+	private String passwordAgain;
+	
+	public Employee() {}
+	
+	public Employee(int id, String firstName, String lastName, String birthDate, String eMail, String password,
+			String passwordAgain) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+		this.eMail = eMail;
+		this.password = password;
+		this.passwordAgain = passwordAgain;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordAgain() {
+		return passwordAgain;
+	}
+
+	public void setPasswordAgain(String passwordAgain) {
+		this.passwordAgain = passwordAgain;
+	}
+	
+}
